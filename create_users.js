192e8178143d7da2a4bd1413a413836900091e97
Daniel Escobar <daescobarp@unal.edu.co>
2020-05-18 10:40:38 -0500

    added more users

--- create_users.js
+++
@@ -1,9 +0,0 @@
-const sqlite3 = require('sqlite3').verbose();
-const db = new sqlite3.Database('database.sqlite3');
-
-db.serialize(() => {
-  db.run('INSERT INTO professors VALUES (?)', "maguzmanp", (err)=>{});
-});
-
-db.close();
-
